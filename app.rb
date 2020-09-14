5ed13e91a9b72c1876e8f11d7e2d6bf4cf8b3183
whychooseaname <68562400+whychooseaname@users.noreply.github.com>
2020-09-14 12:57:01 -0500

    Done.

--- app.rb
+++ app.rb
@@ -3,7 +3,8 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "Welcome to your app!!!! FLATIRON BUILT THIS!!!!
+    Started using shotgun"
   end
 
 end
